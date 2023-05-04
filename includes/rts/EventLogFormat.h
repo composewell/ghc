9669c8b2ff0d25dfa6a873b6a4a0458939f6728d
Harendra Kumar <harendra@composewell.com>
2023-05-04 10:58:44 +0530

    Fix event log size

--- includes/rts/EventLogFormat.h
+++ includes/rts/EventLogFormat.h
@@ -192,7 +192,7 @@
  * ranges higher than this are reserved but not currently emitted by ghc.
  * This must match the size of the EventDesc[] array in EventLog.c
  */
-#define NUM_GHC_EVENT_TAGS        182
+#define NUM_GHC_EVENT_TAGS        202
 
 #if 0  /* DEPRECATED EVENTS: */
 /* we don't actually need to record the thread, it's implicit */
