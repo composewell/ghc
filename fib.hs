a7c6191b68740effd736bd3a58bf0bd6d702e5ec
Adithya Kumar <adithya@composewell.com>
2024-06-12 19:08:20 +0530

    Update the fib example with a more readable combinator

--- fib.hs
+++ fib.hs
@@ -6,12 +6,13 @@ import Control.Concurrent
 import GHC.Stack
 import GHC.Exts
 import GHC.Word
+import Data.Bits
 
 main = do
     -- capabilities <- getNumCapabilities
     -- putStrLn $ "Number of capabilities: " ++ show capabilities
     a  <-  f 20
-    -- triggerProfWindowIO 0 1
+    triggerProf (ReportSince 0 0) True
     -- triggerProfSinceIO 0 9
     b  <-  g 30
     print  $  a  +  b
@@ -22,16 +23,35 @@ main = do
     f n  = fib n
     g n  = fib (n `div` 2)
 
-
 -- The following are equal
--- triggerProfSinceIO i j
--- triggerProfWindowIO i (numGCs - j)
+-- ReportSince j i
+-- ReportWindow (numGCs - j) i
 
-triggerProfSinceIO (W64# a) (W64# b) = IO $
-    \s -> (# triggerProf# (unsafeCoerce# 0#) a (unsafeCoerce# 0#) b s, () #)
+data ReportType
+    = ReportSince
+          { rsOffsetAbsFromBegLower :: Word64
+          , rsOffsetRelFromEndUpper :: Word64
+          }
+    | ReportWindow
+          { rwOffsetRelFromEndLower :: Word64
+          , rwOffsetRelFromEndUpper :: Word64
+          }
 
-triggerProfWindowIO (W64# a) (W64# b) = IO $
-    \s -> (# triggerProf# (unsafeCoerce# 1#) a b (unsafeCoerce# 0#) s, () #)
+triggerProf :: ReportType -> Bool -> IO ()
+triggerProf reportType verbose = do
+    let (W64# unused) = 0
+    case reportType of
+        ReportSince (W64# l) (W64# u) -> IO $ \s ->
+            let (W64# rt) = setVerbosityBit 0
+             in (# triggerProf# (unsafeCoerce# rt) u unused l s, () #)
+        ReportWindow (W64# l) (W64# u) -> IO $ \s ->
+            let (W64# rt) = setVerbosityBit 1
+             in (# triggerProf# (unsafeCoerce# rt) u l unused s, () #)
+    where
+    setVerbosityBit i =
+        if verbose
+        then setBit i 1
+        else i
 
 fib n =
     if n < 2
